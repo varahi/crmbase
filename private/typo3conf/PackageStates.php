c42ad0684374612f9f789b9755edb667bbc57db4
Dmitry Vasilev <dmitry@t3dev.ru>
2022-11-09 11:46:57 +0300

    Change composer.json, add PackageStates.php

---
+++ private/typo3conf/PackageStates.php
@@ -0,0 +1,91 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by TYPO3's package management. Although you can edit it
+# manually, you should rather use the extension manager for maintaining packages.
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return [
+    'packages' => [
+        'core' => [
+            'packagePath' => 'typo3/sysext/core/',
+        ],
+        'extbase' => [
+            'packagePath' => 'typo3/sysext/extbase/',
+        ],
+        'fluid' => [
+            'packagePath' => 'typo3/sysext/fluid/',
+        ],
+        'frontend' => [
+            'packagePath' => 'typo3/sysext/frontend/',
+        ],
+        'fluid_styled_content' => [
+            'packagePath' => 'typo3/sysext/fluid_styled_content/',
+        ],
+        'install' => [
+            'packagePath' => 'typo3/sysext/install/',
+        ],
+        'recordlist' => [
+            'packagePath' => 'typo3/sysext/recordlist/',
+        ],
+        'backend' => [
+            'packagePath' => 'typo3/sysext/backend/',
+        ],
+        'setup' => [
+            'packagePath' => 'typo3/sysext/setup/',
+        ],
+        'rte_ckeditor' => [
+            'packagePath' => 'typo3/sysext/rte_ckeditor/',
+        ],
+        'about' => [
+            'packagePath' => 'typo3/sysext/about/',
+        ],
+        'belog' => [
+            'packagePath' => 'typo3/sysext/belog/',
+        ],
+        'beuser' => [
+            'packagePath' => 'typo3/sysext/beuser/',
+        ],
+        'dashboard' => [
+            'packagePath' => 'typo3/sysext/dashboard/',
+        ],
+        'extensionmanager' => [
+            'packagePath' => 'typo3/sysext/extensionmanager/',
+        ],
+        'filelist' => [
+            'packagePath' => 'typo3/sysext/filelist/',
+        ],
+        'info' => [
+            'packagePath' => 'typo3/sysext/info/',
+        ],
+        'seo' => [
+            'packagePath' => 'typo3/sysext/seo/',
+        ],
+        'tstemplate' => [
+            'packagePath' => 'typo3/sysext/tstemplate/',
+        ],
+        'viewpage' => [
+            'packagePath' => 'typo3/sysext/viewpage/',
+        ],
+        'fancybox' => [
+            'packagePath' => 'typo3conf/ext/fancybox/',
+        ],
+        'gridelements' => [
+            'packagePath' => 'typo3conf/ext/gridelements/',
+        ],
+        'sms_responsive_images' => [
+            'packagePath' => 'typo3conf/ext/sms_responsive_images/',
+        ],
+        'sourceopt' => [
+            'packagePath' => 'typo3conf/ext/sourceopt/',
+        ],
+        'tmpl' => [
+            'packagePath' => 'typo3conf/ext/tmpl/',
+        ],
+        'translate_locallang' => [
+            'packagePath' => 'typo3conf/ext/translate_locallang/',
+        ],
+    ],
+    'version' => 5,
+];
